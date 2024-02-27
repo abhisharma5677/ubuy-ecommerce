6b091ee6978365c29d1942c036865dcc5fb15681
Abhishek Sharma <imabhishek5677@gmail.com>
2024-02-28 03:09:05 +0530

    Changes in CSS

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-white border-t border-gray-200 text-gray-700 py-6 lg:mt-[10px]">
         <div className="container mx-auto text-center">
-            <p>&copy; 2024 Abhishek's Ecommerce Website. All rights reserved.</p>
+            <p>&copy; 2024 Abhishek Ecommerce Website. All rights reserved.</p>
         </div>
     </footer>
   )
